c3112f89c48debe495af38e015fefdb3c892e9b1
Maksim Chuickov <chuickov.k@yandex.ru>
2024-10-14 14:30:59 +0500

    3

---
+++ test_05_formulas/test_05_formulas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_05_formulas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = F(2, 3) + 1 / (F(5, 7));
+            Console.WriteLine(x);   
+            Console.ReadKey();
+        }
+        static double F(double a, double b)
+        {
+            return (Math.Sin(a) + Math.Sin(b)) / (Math.Cos(a) + Math.Cos(b));
+        }
+    }
+}
+
