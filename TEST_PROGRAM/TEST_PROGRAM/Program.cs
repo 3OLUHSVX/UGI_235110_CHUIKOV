09bd4e401db5892baedff00210fc738059c95291
Maksim Chuickov <chuickov.k@yandex.ru>
2025-01-22 10:45:33 +0500

    Задание 7. Экзамен

---
+++ TEST_PROGRAM/TEST_PROGRAM/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        const int limit = 1000;
+        bool[] isGenerated = new bool[limit];
+        // Помечаем числа, которые были порождены
+        for (int i = 1; i < limit; i++)
+        {
+            int generatedNumber = Generate(i);
+            if (generatedNumber < limit)
+            {
+                isGenerated[generatedNumber] = true;
+            }
+            Console.ReadKey();
+        }
+        // Вывод всех самопорождённых чисел
+        Console.WriteLine("Самопорождённые числа меньше 1000:");
+        for (int i = 1; i < limit; i++)
+        {
+            if (!isGenerated[i])
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+    // Функция для порождения числа
+    static int Generate(int number)
+    {
+        int sum = number; while (number > 0)
+        {
+            sum += number % 10; // Добавляем последнюю цифру
+            number /= 10;       // Убираем последнюю цифру
+        }
+            return sum;
+        }
+    }
---
+++ exam_/exam_/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam_
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите натуральное число: "); if (int.TryParse(Console.ReadLine(), out int number) && number > 0)
+            {
+                (int steps, int maxValue) = Posled(number);
+                Console.WriteLine("Шаги:" + steps);
+                Console.WriteLine("Максимальное число:" + maxValue);
+            }
+            else
+            {
+                Console.WriteLine("Введите НАТУРАЛЬНОЕ число");
+            }
+            Console.ReadKey();
+        }
+
+        static (int steps, int maxValue) Posled(int value)
+        {
+            int steps = 1;
+            int maxValue = value;
+            while (value != 1)
+            {
+                if (value % 2 == 0)
+                {
+                    value /= 2;
+                }
+                else
+                {
+                    value = 3 * value + 1;
+                }
+                if (value > maxValue)
+                {
+                    maxValue = value;
+                }
+                steps++;
+            }
+            return (steps, maxValue);
+        }
+    }
+}
